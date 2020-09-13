3a3c7d7118f7bfbf3388bcc804623201a92644a3
razuf <63076299+razuf@users.noreply.github.com>
2020-09-13 08:11:20 +0200

    tests

--- test/real_world_web/live/main_live_test.exs
+++ test/real_world_web/live/main_live_test.exs
@@ -21,6 +21,8 @@ defmodule RealWorldWeb.MainLiveTest do
   }
   @session_id Ecto.UUID.generate()
 
+  @invalid_path "/wrong_path"
+
   defp fixture(conn, :user_login) do
     conn =
       conn
@@ -50,7 +52,7 @@ defmodule RealWorldWeb.MainLiveTest do
     %{conn: conn}
   end
 
-  describe "require_authenticated_user" do
+  describe "standard session if user not signed in " do
     setup [:init_standard_session]
 
     test "shows main page information", %{conn: conn} do
@@ -71,33 +73,50 @@ defmodule RealWorldWeb.MainLiveTest do
       assert {:error, {:redirect, %{to: "/login"}}} == live(conn, "/editor")
       assert {:error, {:redirect, %{to: "/login"}}} == live(conn, "/editor/example_slug")
       assert {:error, {:redirect, %{to: "/login"}}} == live(conn, "/logout")
+
+      assert {:error, {:live_redirect, %{to: "/", kind: :push}}} == live(conn, @invalid_path)
     end
   end
 
-  describe "redirect_if_user_is_authenticated" do
+  describe "redirects if user signed in" do
     setup [:init_user_signed_in_session]
 
-    test "redirects when redirect_if_user_is_authenticated", %{conn: conn} do
+    test "redirects when wrong_path", %{conn: conn} do
+      assert {:error, {:live_redirect, %{to: "/", kind: :push}}} == live(conn, @invalid_path)
       assert {:error, {:redirect, %{to: "/"}}} == live(conn, "/login")
       assert {:error, {:redirect, %{to: "/"}}} == live(conn, "/register")
     end
   end
 
-  describe "click Sign in" do
+  describe "clicks in standard session" do
     setup [:init_standard_session]
 
     test "click Sign in from home", %{conn: conn} do
-      {:ok, live, html} = live(conn, "/login")
+      {:ok, live, html} = live(conn, "/")
       html = clean_html(html)
 
       assert html =~
-               ~s|<li class=\"nav-item \"><a class=\"nav-link\" data-phx-link=\"patch\" data-phx-link-state=\"push\" href=\"/register\"> Sign up </a></li>|
+               ~s|<p>A place to share your Elixir knowledge.</p>|
+
+      assert live
+             |> element("a", "Sign in")
+             |> render_click()
+             |> clean_html() =~
+               ~s|<h1 class=\"text-xs-center\">Sign in</h1><p class=\"text-xs-center\"><a class=\"nav-link\" data-phx-link=\"patch\" data-phx-link-state=\"push\" href=\"/register\">Need an account?</a></p>|
+    end
+
+    test "click Register in from home", %{conn: conn} do
+      {:ok, live, html} = live(conn, "/")
+      html = clean_html(html)
+
+      assert html =~
+               ~s|<p>A place to share your Elixir knowledge.</p>|
 
       assert live
              |> element("a", "Sign up")
              |> render_click()
              |> clean_html() =~
-               ~s|<li class=\"nav-item active\"><a class=\"nav-link\" data-phx-link=\"patch\" data-phx-link-state=\"push\" href=\"/register\"> Sign up </a></li>|
+               ~s|<h1 class=\"text-xs-center\">Sign up</h1><p class=\"text-xs-center\"><a class=\"nav-link\" data-phx-link=\"patch\" data-phx-link-state=\"push\" href=\"/login\">Have an account?</a></p|
     end
   end
 
